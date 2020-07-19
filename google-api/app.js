69c34c0e68fb79e46aba8ea9213786fb153cdf9b
Firah Putri <firahputripratiwi10@gmail.com>
2020-07-19 21:18:36 +0700

    penambahan pada rekam-jejak

--- google-api/app.js
+++ google-api/app.js
@@ -1,157 +1,98 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -7.3266396, lng: 108.2258899 },
-    zoom: 13
+    center: { lat: 51.0967619, lng: 5.9664093 },
+    zoom: 6
   });
-  var myLatLng = {lat:-7.3000398, lng: 108.2025775};
-  var marker_saungranggon = new google.maps.Marker({
+  var myLatLng = {lat: 50.9294336, lng: 11.587482};
+  var marker_jena = new google.maps.Marker({
     position: myLatLng,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: 'Saung Ranggon'
+    title: 'Universitas Jena'
 
   });
-      var infowindow_saungranggon = new google.maps.InfoWindow({
-      content: '<h1>Saung Ranggon</h1><p>Jl. Leuwi Dahu No.81, Indihiang, Kec. Indihiang, Tasikmalaya, Jawa Barat 46151</p><p><i>+6281320574300<i></p>'
+      var infowindow_jena = new google.maps.InfoWindow({
+      content: '<h1>Universitas Jena</h1><p>Fürstengraben 1, 07743 Jena, Jerman</p><p><i>+49364194000<i></p>'
     });
-   marker_saungranggon.addListener('click', function() {
-      infowindow_saungranggon.open(map, marker_saungranggon);
+   marker_jena.addListener('click', function() {
+      infowindow_jena.open(map, marker_jena);
     });
 
-  var rmsutsen = {lat:-7.325795, lng: 108.2182767};
-  var marker_rmsutsen = new google.maps.Marker({
-    position: rmsutsen,
+  var stad = {lat:50.8759021, lng: 12.0795514};
+  var marker_stad = new google.maps.Marker({
+    position: stad,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: 'Rumah Makan Sutsen'
+    title: 'Stadtverwaltung Gera'
     
   });
-    var infowindow_rmsutsen = new google.maps.InfoWindow({
-        content: '<h1>Rumah Makan Sutsen</h1><p>Jl. Sutisna Senjaya No.163, Cikalang, Kec. Tawang, Tasikmalaya, Jawa Barat 46111</p><p><i>+622652354990<i></p>'
+    var infowindow_stad = new google.maps.InfoWindow({
+        content: '<h1>Stadtverwaltung Gera</h1><p>Kornmarkt 12, 07545 Gera, Jerman</p><p><i>+493658380<i></p>'
       });
-     marker_rmsutsen.addListener('click', function() {
-        infowindow_rmsutsen.open(map, marker_rmsutsen);
+     marker_stad.addListener('click', function() {
+        infowindow_stad.open(map, marker_stad);
       });
 
-  var ninianteh = {lat:-7.3239909, lng: 108.2209364};
-  var marker_ninianteh = new google.maps.Marker({
-    position: ninianteh,
+  var lohse = {lat:51.7971929, lng: 7.8141562};
+  var marker_lohse = new google.maps.Marker({
+    position: lohse,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: 'Rumah Makan Nini Anteh'
+    title: 'Kleintierpraxis Lohse/Feige'
     
   });
-    var infowindow_ninianteh = new google.maps.InfoWindow({
-        content: '<h1>Rumah Makan Nini Anteh</h1><p>Jl. Dewi Sartika No.14, Empangsari, Kec. Tawang, Tasikmalaya, Jawa Barat 46113</p><p><i>+62265314410<i></p>'
+    var infowindow_lohse = new google.maps.InfoWindow({
+        content: '<h1>Kleintierpraxis Lohse/Feige</h1><p>Ronneburger Str. 74, 07546 Gera, Jerman</p><p><i>+4936555176036<i></p>'
       });
-     marker_ninianteh.addListener('click', function() {
-        infowindow_ninianteh.open(map, marker_ninianteh);
+     marker_lohse.addListener('click', function() {
+        infowindow_lohse.open(map, marker_lohse);
       });
 
-  var brotfabrik = {lat:-7.3270177, lng: 108.232465};
-  var marker_brotfabrik = new google.maps.Marker({
-    position: brotfabrik,
+  var burger = {lat:-7.3270177, lng: 108.232465};
+  var marker_burger = new google.maps.Marker({
+    position: burger,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: 'Berliner Brotfabrik Konditorei & Cafe'
+    title: 'Bürgergalerie Neumünster e.V.'
     
   });
-       var infowindow_brotfabrik = new google.maps.InfoWindow({
-        content: '<h1>Berliner Brotfabrik Konditorei & Cafe</h1><p>Jalan Gunung Sari No 25 Sukasari, Lengkongsari, Kec. Tawang, Tasikmalaya, Jawa Barat 461113</p><p><i>+6281322088480<i></p>'
+       var infowindow_burger = new google.maps.InfoWindow({
+        content: '<h1>Bürgergalerie Neumünster e.V.</h1><p>Esplanade 20, 24534 Neumünster, Jerman</p>'
       });
-     marker_brotfabrik.addListener('click', function() {
-        infowindow_brotfabrik.open(map, marker_brotfabrik);
+     marker_burger.addListener('click', function() {
+        infowindow_burger.open(map, marker_burger);
       });
    
-  var asepstroberi = {lat:-7.3256324, lng: 108.2173323};
-  var marker_asepstroberi = new google.maps.Marker({
-    position: asepstroberi,
+  var Geraer = {lat:50.874083, lng: 12.0758121};
+  var marker_geraer = new google.maps.Marker({
+    position: Geraer,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: 'Liwet Aseo Stroberi'
+    title: 'Geraer Volkshochschule Aenne Biermann'
     
   });
-       var infowindow_asepstroberi = new google.maps.InfoWindow({
-        content: '<h1>Liwet Asep Stroberi</h1><p>Jl. R. Ikik Wiradikarta No.9, Yudanagara, Kec. Cihideung, Tasikmalaya, Jawa Barat 46112</p><p><i>+6281220317111<i></p>'
+       var infowindow_geraer = new google.maps.InfoWindow({
+        content: '<h1>Geraer Volkshochschule Aenne Biermann</h1><p>Talstraße 3, 07545 Gera, Jerman</p><p><i>+4936555259301<i></p>'
       });
-     marker_asepstroberi.addListener('click', function() {
-        infowindow_asepstroberi.open(map, marker_asepstroberi);
+     marker_geraer.addListener('click', function() {
+        infowindow_geraer.open(map, marker_geraer);
       });
 
-  var saungjati = {lat:-7.3459886, lng: 108.2353685};
-  var marker_saungjati = new google.maps.Marker({
-    position: saungjati,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Rumah Makan Saung Gunung Jati'
-    
-  });
-       var infowindow_saungjati = new google.maps.InfoWindow({
-        content: '<h1>Rumah Makan Saung Gunung Jati</h1><p>Jl. Letjen Mashudi No.45, Setiaratu, Kec. Cibeureum, Tasikmalaya, Jawa Barat 46123</p><p><i>+6285223658625<i></p>'
-      });
-     marker_saungjati.addListener('click', function() {
-        infowindow_saungjati.open(map, marker_saungjati);
-      });
 
-  var sotononoy = {lat:-7.3345004, lng: 108.2347774};
-  var marker_sotononoy = new google.maps.Marker({
-    position: sotononoy,
+  var thur = {lat:50.9230058, lng: 10.1443439};
+  var marker_thur = new google.maps.Marker({
+    position: thur,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: 'Soto Ayam Kampung Nonoy'
+    title: 'Thuringian Parliament'
     
   });
-       var infowindow_sotononoy = new google.maps.InfoWindow({
-        content: '<h1>Soto Ayam Kampung Nonoy</h1><p>Samping Al - Mutaqin, Jl. Sutisna Senjaya No.240, Cikalang, Kec. Tawang, Tasikmalaya, Jawa Barat 46114</p><p><i>+6287745573197<i></p>'
+       var infowindow_thur = new google.maps.InfoWindow({
+        content: '<h1>Thuringian Parliament</h1><p>Jürgen-Fuchs-Straße 1, 99096 Erfurt, Jerman</p><p><i>+493613772200<i></p>'
       });
-     marker_sotononoy.addListener('click', function() {
-        infowindow_sotononoy.open(map, marker_sotononoy);
-      });
-
-  var hegarsari = {lat:-7.3442951, lng: 108.2210337};
-  var marker_hegarsari = new google.maps.Marker({
-    position: hegarsari,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Saung Hegarsari'
-    
-  });
-       var infowindow_hegarsari = new google.maps.InfoWindow({
-        content: '<h1>Saung Hegarsari</h1><p>Jl. Bkr No.68, Kahuripan, Kec. Tawang, Tasikmalaya, Jawa Barat 46114</p><p><i>+62265329099<i></p>'
-      });
-     marker_hegarsari.addListener('click', function() {
-        infowindow_hegarsari.open(map, marker_hegarsari);
-      });
-
-  var enakdiuk = {lat:-7.3070463, lng: 108.1861492};
-  var marker_enakdiuk = new google.maps.Marker({
-    position: enakdiuk,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Rumah Makan Genah Calik'
-    
-  });
-       var infowindow_enakdiuk = new google.maps.InfoWindow({
-        content: '<h1>Rumah Makan Genah Calik</h1><p>Jl. Ir. H. Juanda No.14, Panyingkiran, Kec. Indihiang, Tasikmalaya, Jawa Barat 46211</p><p><i>+62852205962679<i></p>'
-      });
-     marker_enakdiuk.addListener('click', function() {
-        infowindow_enakdiuk.open(map, marker_enakdiuk);
-      });
-
-  var wongsolo = {lat:-7.3440752, lng: 108.2057127};
-  var marker_wongsolo = new google.maps.Marker({
-    position: wongsolo,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'RM Wong Solo Tasikmalaya'
-    
-  });
-       var infowindow_wongsolo = new google.maps.InfoWindow({
-        content: '<h1>RM Wong Solo Tasikmalaya</h1><p>Jl. Mesjid Agung No.12, Tawangsari, Kec. Tawang, Tasikmalaya, Jawa Barat 46121</p><p><i>+62265335455<i></p>'
-      });
-     marker_wongsolo.addListener('click', function() {
-        infowindow_wongsolo.open(map, marker_wongsolo);
+     marker_thur.addListener('click', function() {
+        infowindow_thur.open(map, marker_thur);
       });
 
 }
